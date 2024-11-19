7381a1ddb97b32d284d9a84bf19b1b113bd23953
vendor app ui <hostinvosting3@gmail.com>
2024-11-19 17:00:31 +0530

    weather current api

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -3,8 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class WeatherController extends Controller
 {
-    //
+    public function getCurrentWeather(Request $request){
+        $apiUrl = "http://api.weatherapi.com/v1/current.json";
+        $apiParams = [
+            'key' => 'c4535de2be074b6d9f2101903241811',
+            'q' => $request->query('place'),
+            'limit' => 9670,
+        ];
+
+        // Build API URL with query parameters
+        $apiUrlWithParams = $apiUrl . '?' . http_build_query($apiParams);
+
+        // Fetch data from API using Laravel's HTTP Client
+        $response = Http::get($apiUrlWithParams);
+
+        // Check if response is valid
+        if ($response->failed()) {
+            return response()->json(['error' => 'Unable to fetch data from API.'], 500);
+        }
+        // Decode JSON into PHP array
+        $data = $response->json();
+
+        // Return filtered data as JSON
+        return response()->json($data);
+    }
 }
